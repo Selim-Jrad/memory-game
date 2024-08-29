9a08e535d3c33aed5be906ec5c6c1d64c45e6f58
selim jrad <141447297+Selim-Jrad@users.noreply.github.com>
2024-08-29 18:01:07 +0200

    Update jav.js

--- jav.js
+++ jav.js
@@ -3,51 +3,51 @@ document.addEventListener('DOMContentLoaded', () => {
   const cardArray = [
     {
       name: 'Adiamonds',
-      img: 'pics/Adiamonds.png'
+      img: 'Adiamonds.png'
     },
     {
       name: 'Ahearts',
-      img: 'pics/Ahearts.png'
+      img: 'Ahearts.png'
     },
     {
       name: 'Jhearts',
-      img: 'pics/Jhearts.png'
+      img: 'Jhearts.png'
     },
     {
       name: 'mfw_imoutofideas',
-      img: 'pics/mfw_imoutofideas.png'
+      img: 'mfw_imoutofideas.png'
     },
     {
       name: 'Ihatemylife',
-      img: 'pics/Ihatemylife.png'
+      img: 'Ihatemylife.png'
     },
     {
       name: 'Aclubs',
-      img: 'pics/Aclubs.png'
+      img: 'Aclubs.png'
     },
     {
       name: 'Adiamonds',
-      img: 'pics/Adiamonds.png'
+      img: 'Adiamonds.png'
     },
     {
       name: 'Ahearts',
-      img: 'pics/Ahearts.png'
+      img: 'Ahearts.png'
     },
     {
       name: 'Jhearts',
-      img: 'pics/Jhearts.png'
+      img: 'Jhearts.png'
     },
     {
       name: 'mfw_imoutofideas',
-      img: 'pics/mfw_imoutofideas.png'
+      img: 'mfw_imoutofideas.png'
     },
     {
       name: 'Ihatemylife',
-      img: 'pics/Ihatemylife.png'
+      img: 'Ihatemylife.png'
     },
     {
       name: 'Aclubs',
-      img: 'pics/Aclubs.png'
+      img: 'Aclubs.png'
     }
   ]
 
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
-      card.setAttribute('src', 'pics/blank.png')
+      card.setAttribute('src', 'blank.png')
       card.setAttribute('data-id', i)
       card.addEventListener('click', flipCard)
       grid.appendChild(card)
@@ -77,20 +77,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const optionTwoId = cardsChosenId[1]
     
     if(optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', 'pics/blank.png')
-      cards[optionTwoId].setAttribute('src', 'pics/blank.png')
+      cards[optionOneId].setAttribute('src', 'blank.png')
+      cards[optionTwoId].setAttribute('src', 'blank.png')
       alert('You have clicked the same image!')
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match')
-      cards[optionOneId].setAttribute('src', 'pics/white.png')
-      cards[optionTwoId].setAttribute('src', 'pics/white.png')
+      cards[optionOneId].setAttribute('src', 'white.png')
+      cards[optionTwoId].setAttribute('src', 'white.png')
       cards[optionOneId].removeEventListener('click', flipCard)
       cards[optionTwoId].removeEventListener('click', flipCard)
       cardsWon.push(cardsChosen)
     } else {
-      cards[optionOneId].setAttribute('src', 'pics/blank.png')
-      cards[optionTwoId].setAttribute('src', 'pics/blank.png')
+      cards[optionOneId].setAttribute('src', 'blank.png')
+      cards[optionTwoId].setAttribute('src', 'blank.png')
       alert('Sorry, try again')
     }
     cardsChosen = []
@@ -101,7 +101,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  //flip your card
+  //flip your card, also, no words created by god can describe the amount of stress i had making this game, i hate it and i hate this code
+  //but i really hope you like it fr fr
+  //plz like it
+  //i need you to like it
+  //alse pepe is here
   function flipCard() {
     let cardId = this.getAttribute('data-id')
     cardsChosen.push(cardArray[cardId].name)
